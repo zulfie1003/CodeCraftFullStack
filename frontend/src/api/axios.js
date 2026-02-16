c3d41f34890350984eb19ef70836d89d847967ab
zulfie <alijulfekar419@gmail.com>
2026-02-16 21:41:36 +0530

    Update frontend axios for production API

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001/api",
   headers: {
     "Content-Type": "application/json"
   }
@@ -17,4 +17,3 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
-
